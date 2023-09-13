95c9ce988cf1278778130d45c841605447801b53
Hercapo <hercapo.wc@gmail.wc>
2023-09-13 16:46:02 -0300

    desafio refactor de login

---
+++ src/dao/models/users.manager.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const collection = 'Users';
+
+const schema = new mongoose.Schema({
+    first_name:String,
+    last_name:String,
+    email:String,
+    age:Number,
+    password:String,
+    githubProfile: String,
+    role: { type: String, default: function() {
+        if(this.email === "adminCoder@coder.com") {
+            return "Admin";
+        }
+        return "Usuario";
+    } }
+})
+
+const userModel = mongoose.model(collection,schema);
+
+export default userModel;
